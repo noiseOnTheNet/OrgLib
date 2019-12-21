7701153945326ccd4e98f62a9fb7204069d90b4d
Marco Paolo Valerio Vezzoli <mvezzoli@micron.com>
2019-12-21 09:22:48 +0100

    more fix

--- main.rs
+++ main.rs
@@ -12,31 +12,31 @@ fn add_child2(parent: &mut Node, child : Node){
 
 #[cfg(test)]
 mod tests {
-  use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
-// The output is wrapped in a Result to allow matching on errors
-// Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
- 
-#[test]
-fn prova(){
-  let mut lines_count : usize = 0;
-  if let Ok(lines) = read_lines("./test1.org") {
-        // Consumes the iterator, returns an (Optional) String
-        for line in lines {
-            if let Ok(ip) = line {
-                lines_count += 1;
+    use std::fs::File;
+    use std::io::{self, BufRead};
+    use std::path::Path;
+    // The output is wrapped in a Result to allow matching on errors
+    // Returns an Iterator to the Reader of the lines of the file.
+    fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+    where P: AsRef<Path>, {
+	let file = File::open(filename)?;
+	Ok(io::BufReader::new(file).lines())
+    }
+    
+    #[test]
+    fn prova(){
+	let mut lines_count : usize = 0;
+	if let Ok(lines) = read_lines("./test1.org") {
+            // Consumes the iterator, returns an (Optional) String
+            for line in lines {
+		if let Ok(ip) = line {
+                    lines_count += 1;
+		}
             }
-        }
-  }
-  crate::main();
-  assert!(lines_count>0);
-}
+	}
+	crate::main();
+	assert!(lines_count>0);
+    }
 }
 
 fn main() {
