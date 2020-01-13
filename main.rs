c2faebf591de8700807be46fe45c84b2afa72e46
Marco Paolo Valerio Vezzoli <mvezzoli@micron.com>
2020-01-13 17:44:05 +0100

    some cosmetics

--- main.rs
+++ main.rs
@@ -1,53 +1,48 @@
 
 mod org;
-use org::{Node,Status};
-
-
-fn add_child2(parent: &mut Node, child : Node){
-    &parent.children.push(child);
-}
-
-
-
+use org::{Node, Status};
 
 #[cfg(test)]
 mod tests {
-    use std::fs::File;
-    use std::io::{self, BufRead};
-    use std::path::Path;
-    // The output is wrapped in a Result to allow matching on errors
-    // Returns an Iterator to the Reader of the lines of the file.
-    fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-    where P: AsRef<Path>, {
-	let file = File::open(filename)?;
-	Ok(io::BufReader::new(file).lines())
-    }
-    
-    #[test]
-    fn prova(){
-	let mut lines_count : usize = 0;
-	if let Ok(lines) = read_lines("./test1.org") {
-            // Consumes the iterator, returns an (Optional) String
-            for line in lines {
-		if let Ok(ip) = line {
-                    lines_count += 1;
-		}
-            }
+	use std::fs::File;
+	use std::io::{self, BufRead};
+	use std::path::Path;
+	// The output is wrapped in a Result to allow matching on errors
+	// Returns an Iterator to the Reader of the lines of the file.
+	fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+	where
+		P: AsRef<Path>,
+	{
+		let file = File::open(filename)?;
+		Ok(io::BufReader::new(file).lines())
 	}
-	crate::main();
-	assert!(lines_count>0);
-    }
+
+	#[test]
+	fn prova() {
+		let mut lines_count: usize = 0;
+		if let Ok(lines) = read_lines("./test1.org") {
+			// Consumes the iterator, returns an (Optional) String
+			for line in lines {
+				if let Ok(_ip) = line {
+					lines_count += 1;
+				}
+			}
+		}
+		crate::main();
+		assert!(lines_count > 0);
+	}
+
 }
 
 fn main() {
-  let mut x = Node::new(1,"hi".to_string());
-  let mut y = Node::new(2,"there".to_string());
-  let mut z = Node::new(2,"how".to_string());
-  let mut w = Node::new(3,"are".to_string());
-  y.add_child(w);
-  x.add_child(y);
-  x.add_child(z);
-  x.status = Some(Status::TODO);
-  println!("Debug {:?}", x);
-  println!("Display {}", x);
+	let mut x = Node::new(1, "hi".to_string());
+	let mut y = Node::new(2, "there".to_string());
+	let z = Node::new(2, "how".to_string());
+	let w = Node::new(3, "are".to_string());
+	y.add_child(w).unwrap();
+	x.add_child(y).unwrap();
+	x.add_child(z).unwrap();
+	x.status = Some(Status::TODO);
+	println!("Debug {:?}", x);
+	println!("Display {}", x);
 }
--- org.rs
+++ org.rs
@@ -1,119 +1,123 @@
 use std::fmt;
 #[derive(Debug)]
-pub enum Status {TODO , WIP , DONE}
+pub enum Status {
+	TODO,
+	WIP,
+	DONE,
+}
 
-fn display_option_status(s : &Option<Status>) -> &'static str{
-  match *s {
-    None => "",
-    Some(Status::TODO) => "TODO ",
-    Some(Status::WIP) => "WIP ",
-    Some(Status::DONE) => "DONE ",
-  }
+fn display_option_status(s: &Option<Status>) -> &'static str {
+	match *s {
+		None => "",
+		Some(Status::TODO) => "TODO ",
+		Some(Status::WIP) => "WIP ",
+		Some(Status::DONE) => "DONE ",
+	}
 }
 
 #[derive(Debug)]
 enum Month {
-  Jan,
-  Feb,
-  Mar,
-  Apr,
-  May,
-  Jun,
-  Jul,
-  Aug,
-  Sep,
-  Oct,
-  Nov,
-  Dec
+	Jan,
+	Feb,
+	Mar,
+	Apr,
+	May,
+	Jun,
+	Jul,
+	Aug,
+	Sep,
+	Oct,
+	Nov,
+	Dec,
 }
 
 #[derive(Debug)]
-struct Date{
-  year : u16,
-  month : Month,
-  day : u16
+struct Date {
+	year: u16,
+	month: Month,
+	day: u16,
 }
 
 #[derive(Debug)]
-struct Time{
-  hour : u16,
-  min : u16
+struct Time {
+	hour: u16,
+	min: u16,
 }
 
 #[derive(Debug)]
 enum RepeatInterval {
-  Year,
-  Month,
-  Week,
-  Day
+	Year,
+	Month,
+	Week,
+	Day,
 }
 
 #[derive(Debug)]
 struct Repeat {
-  mult : u16,
-  interval : RepeatInterval,
-  skip : bool
+	mult: u16,
+	interval: RepeatInterval,
+	skip: bool,
 }
 
 #[derive(Debug)]
-pub struct Timestamp{
-  date : Date,
-  time : Option<Time>,
-  repeat : Option<Repeat>
+pub struct Timestamp {
+	date: Date,
+	time: Option<Time>,
+	repeat: Option<Repeat>,
 }
 
 #[derive(Debug)]
-pub struct Node{
-  pub level : usize,
-  pub title : String,
-  pub scheduled : Option<Timestamp>,
-  pub deadline : Option<Timestamp>,
-  pub text : String,
-  pub status : Option<Status>,
-  pub children : Vec<Node>
+pub struct Node {
+	pub level: usize,
+	pub title: String,
+	pub scheduled: Option<Timestamp>,
+	pub deadline: Option<Timestamp>,
+	pub text: String,
+	pub status: Option<Status>,
+	pub children: Vec<Node>,
 }
 
-impl Node{
-  pub fn add_child(self: &mut Node, child : Node) -> Result<(),String>{
-    if self.level < child.level{
-      &self.children.push(child);
-      Ok(())
-    }else{
-      Err(format!("level {} of the child node is not higher than level {} of the parent node",child.level,self.level))
-    }
-    
-  }
-  pub fn format(self: &Node) -> String{
-    let subnodes = &self.children;
-    let status = &self.status;
-    let subtext: Vec<String> = subnodes.into_iter().map(|x| x.format()).collect();
-    format!(
-      "{} {}{}\n{}",
-      "*".repeat(self.level),
-      display_option_status(status),
-      self.title,subtext.join("")
-      )
-  }
-  pub fn new(level : usize, title : String) -> Node{
-    Node{
-      level: level,
-      title: title,
-      scheduled : None,
-      deadline : None,
-      text: String::from(""),
-      status: None,
-      children: Vec::new()
-    }
-  }
+impl Node {
+	pub fn add_child(self: &mut Node, child: Node) -> Result<(), String> {
+		if self.level < child.level {
+			&self.children.push(child);
+			Ok(())
+		} else {
+			Err(format!("level {} of the child node is not higher than level {} of the parent node",child.level,self.level))
+		}
+	}
+	pub fn format(self: &Node) -> String {
+		let subnodes = &self.children;
+		let status = &self.status;
+		let subtext: Vec<String> = subnodes.into_iter().map(|x| x.format()).collect();
+		format!(
+			"{} {}{}\n{}",
+			"*".repeat(self.level),
+			display_option_status(status),
+			self.title,
+			subtext.join("")
+		)
+	}
+	pub fn new(level: usize, title: String) -> Node {
+		Node {
+			level: level,
+			title: title,
+			scheduled: None,
+			deadline: None,
+			text: String::from(""),
+			status: None,
+			children: Vec::new(),
+		}
+	}
 }
 
 impl fmt::Display for Node {
-    // This trait requires `fmt` with this exact signature.
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // Write strictly the first element into the supplied output
-        // stream: `f`. Returns `fmt::Result` which indicates whether the
-        // operation succeeded or failed. Note that `write!` uses syntax which
-        // is very similar to `println!`.
-        write!(f, "{}", self.format())
-    }
+	// This trait requires `fmt` with this exact signature.
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		// Write strictly the first element into the supplied output
+		// stream: `f`. Returns `fmt::Result` which indicates whether the
+		// operation succeeded or failed. Note that `write!` uses syntax which
+		// is very similar to `println!`.
+		write!(f, "{}", self.format())
+	}
 }
